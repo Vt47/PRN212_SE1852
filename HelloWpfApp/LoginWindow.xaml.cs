13fd9d5bc895d4d94d5416cc9ade603c3c1326bf
Vt47 <havietthanh3003@gmail.com>
2025-06-13 08:31:54 +0700

    buttunControler_TongHop

---
+++ HelloWpfApp/LoginWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWpfApp
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+            //gia lap account
+            //neu username obama va pass là 123
+            //thì cho vào màn hình đăng nhập
+            if (txtUserName.Text == "obama" && txtPassword.Password == "123")
+            {
+                
+                MainWindow mw = new MainWindow();
+                mw.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng");
+            }
+        }
+    }
+}
